ee629aea41450a6527cbfe4a8be1582af39e0e85
Alex <opiemike613@yahoo.com>
2019-06-18 16:59:03 -0500

    Try without js

--- javascript/web-loaders.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function loadPNG(pngURL: string, callback: (image: HTMLImageElement) => void): void;
-export declare function loadJSON(jsonURL: string, callback: (json: any) => void): void;
