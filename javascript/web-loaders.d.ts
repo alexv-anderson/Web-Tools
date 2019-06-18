a97946137cdf19056d029abf5efe0fe58fe9744c
Alex <opiemike613@yahoo.com>
2019-06-18 11:01:51 -0500

    Draft package

---
+++ javascript/web-loaders.d.ts
@@ -0,0 +1,2 @@
+export declare function loadPNG(pngURL: string, callback: (image: HTMLImageElement) => void): void;
+export declare function loadJSON(jsonURL: string, callback: (json: any) => void): void;
